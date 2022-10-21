27cdb953baba7ac50625c51336f115a1bbab9991
ioni5 <ioni5.goto@gmail.com>
2022-10-21 11:37:50 -0500

    refactor

--- src/main/java/io/github/ioni5/Console.java
+++ src/main/java/io/github/ioni5/Console.java
@@ -5,5 +5,14 @@ public class Console {
     public void write(String message) {
         System.out.print("\n" + message);
     }
+
+    public void writeWithDelay(String message) {
+        this.write(message);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.exit(0);
+        }
+    }
     
 }
--- src/main/java/io/github/ioni5/Game.java
+++ src/main/java/io/github/ioni5/Game.java
@@ -4,8 +4,7 @@ public class Game {
     
     public void start() throws InterruptedException {
         Console console = new Console();
-        console.write("You find yourself standing in an open field, filled with grass and yellow wildflowers.");
-        Thread.sleep(2000);
-        console.write("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.");
+        console.writeWithDelay("You find yourself standing in an open field, filled with grass and yellow wildflowers.");
+        console.writeWithDelay("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.");
     }
 }
