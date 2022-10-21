b4c8e84074db66bc1c0cb9a9fdece1f2df57242b
ioni5 <ioni5.goto@gmail.com>
2022-10-21 11:26:56 -0500

    refactor

---
+++ src/main/java/io/github/ioni5/Console.java
@@ -0,0 +1,9 @@
+package io.github.ioni5;
+
+public class Console {
+
+    public void write(String message) {
+        System.out.print("\n" + message);
+    }
+    
+}
---
+++ src/main/java/io/github/ioni5/Game.java
@@ -0,0 +1,10 @@
+package io.github.ioni5;
+
+public class Game {
+    
+    public void start() {
+        Console console = new Console();
+        console.write("You find yourself standing in an open field, filled with grass and yellow wildflowers.");
+        console.write("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.");
+    }
+}
